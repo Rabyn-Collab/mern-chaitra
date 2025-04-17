02105a1407db61e68eefc639c470b7985d7fb2ff
Rabyn-Collab <babynshrestha64@gmail.com>
2025-04-17 08:03:38 +0545

    changes

--- src/pages/home/TechSec.jsx
+++ src/pages/home/TechSec.jsx
@@ -9,7 +9,9 @@ export default function TechSec() {
 
       <div className='grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-10 justify-items-center'>
 
-        <DiPython size={200} />
+
+        {/* transition: all 2s ease-in 0s; */}
+        <DiPython size={200} className='hover:scale-125  duration-200 ease-in-out delay-75 hover:text-pink-400 cursor-pointer' />
         <DiCss3 size={200} />
         <DiJavascript1 size={200} />
         <DiReact size={200} />
