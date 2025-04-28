7c4b2a2541002904a8611df0745c23f12b9b1545
Rabyn-Collab <babynshrestha64@gmail.com>
2025-04-28 08:00:45 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,104 +1,52 @@
-import React from 'react'
-import { SimpleCard } from '../../components/SimpleCard'
-import { CardDefault } from '../../components/CardDefault';
-
-
-
-
-
-const posts = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-  }
-];
-
-
-const recipes = [
-  {
-    "id": 1,
-    "name": "Classic Margherita Pizza",
-    "ingredients": [
-      "Pizza dough",
-      "Tomato sauce",
-      "Fresh mozzarella cheese",
-      "Fresh basil leaves",
-      "Olive oil",
-      "Salt and pepper to taste"
-    ],
-    "instructions": [
-      "Preheat the oven to 475°F (245°C).",
-      "Roll out the pizza dough and spread tomato sauce evenly.",
-      "Top with slices of fresh mozzarella and fresh basil leaves.",
-      "Drizzle with olive oil and season with salt and pepper.",
-      "Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.",
-      "Slice and serve hot."
-    ],
-    "image": "https://cdn.dummyjson.com/recipe-images/1.webp",
-    "rating": 4.6,
-  },
-  {
-    "id": 2,
-    "name": "Vegetarian Stir-Fry",
-    "ingredients": [
-      "Tofu, cubed",
-      "Broccoli florets",
-      "Carrots, sliced",
-      "Bell peppers, sliced",
-      "Soy sauce",
-      "Ginger, minced",
-      "Garlic, minced",
-      "Sesame oil",
-      "Cooked rice for serving"
-    ],
-    "instructions": [
-      "In a wok, heat sesame oil over medium-high heat.",
-      "Add minced ginger and garlic, sauté until fragrant.",
-      "Add cubed tofu and stir-fry until golden brown.",
-      "Add broccoli, carrots, and bell peppers. Cook until vegetables are tender-crisp.",
-      "Pour soy sauce over the stir-fry and toss to combine.",
-      "Serve over cooked rice."
-    ],
-    "image": "https://cdn.dummyjson.com/recipe-images/2.webp",
-    "rating": 4.7,
-
-  },
-];
+import { faker } from '@faker-js/faker';
+import { Button, IconButton, Typography } from '@material-tailwind/react';
+import React, { useState } from 'react'
 
 export default function Home() {
 
 
-  // const n1 = [11, 22, 33, 44, 55];
-  // const n2 = [1, 2, 3, 4, 5];
+  const [users, setUsers] = useState([]);
 
-  // console.log([...n1, ...n2]);
+  const handleUser = () => {
+    const user = {
+      userId: faker.string.uuid(),
+      username: faker.internet.username(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+    };
+    setUsers((prev) => [...prev, user]);
+  }
+
+  const removeUser = (userId) => {
+    setUsers((prev) => prev.filter((user) => user.userId !== userId));
+  }
 
 
   return (
-    <div className='p-5 '>
+    <div>
+      <div className='sticky top-0 bg-amber-100 p-3'>
+        <Button onClick={handleUser} color='purple'>Add User</Button>
+      </div>
 
-      {/* {posts.map((post) => {
-        return <SimpleCard post={post} key={post.id} />;
-      })} */}
+      <div className='grid grid-cols-5 gap-10 p-5'>
 
-      {recipes.map((recipe) => {
-        return <CardDefault recipe={recipe} key={recipe.id} />
-      })}
 
+        {users.map((user) => {
+          return <div key={user.userId} className='mt-5 space-y-2'>
+            <img className='h-[200px] w-full object-cover' src={user.avatar} alt="" />
+            <Typography variant='h5'>{user.username}</Typography>
+            <div className='flex justify-between flex-wrap gap-3'>
+              <p>{user.email}</p>
+              <IconButton onClick={() => removeUser(user.userId)} color='pink' size='sm' >
+                <i className='fas fa-trash'></i>
+              </IconButton>
+            </div>
+
+
+          </div>
+
+        })}
+      </div>
 
 
 
