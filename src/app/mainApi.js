2740a1975dd961efe010663c0e99b92731784461
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-27 07:59:32 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 
-//export const baseUrl = 'http://192.168.194.1:5000';
+//export const baseUrl = 'http://192.168.194.1:5000';//
 export const baseUrl = 'https://mern-chaitra.onrender.com';
 
 export const mainApi = createApi({
